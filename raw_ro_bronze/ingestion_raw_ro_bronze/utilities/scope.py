85383f5d9993bf2a6df258ad149ccb67253dcc06
AirtonLira <airtonlirajr@gmail.com>
2026-02-02 08:02:01 +0000

    ride key

--- raw_ro_bronze/ingestion_raw_ro_bronze/utilities/scope.py
+++ raw_ro_bronze/ingestion_raw_ro_bronze/utilities/scope.py
@@ -1,4 +1,5 @@
 from databricks.sdk import WorkspaceClient
+import os
 
 w = WorkspaceClient()
 
@@ -7,4 +8,4 @@ scope_name = "openrouter"
 w.secrets.create_scope(scope=scope_name)
 
 # criar secret dentro do escopo
-w.secrets.put_secret(scope_name, "minha-key", string_value="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")
+w.secrets.put_secret(scope_name, "minha-key", string_value=os.getenv("OPENROUTER_API_KEY)
